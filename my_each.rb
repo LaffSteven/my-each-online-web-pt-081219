220e4cb66c8c95b8016a78b403d391eedd0368c9
Steven Laff <steve.laff@gmail.com>
2019-08-20 01:50:49 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,7 +1,7 @@
 def my_each(array) 
   counter = 0
-  while counter < array.length do
-    array[counter]
+  while counter < array.length
+    yield (array[counter])
     counter += 1
   end
   array
